3e928ae5aa48b8d401311dd91eb4cd01722e271b
MayurShirbhate <96284376+MayurShirbhate@users.noreply.github.com>
2025-03-30 20:59:42 +0530

    Update server.js

--- bezkoder-app/server.js
+++ bezkoder-app/server.js
@@ -35,7 +35,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to INDIA." });
+  res.json({ message: "Welcome to India." });
 });
 
 require("./app/routes/turorial.routes")(app);
